d91e78d8d1e8386491c8f8314dd942bec07f10de
Haoshui Gao <haoshui.gao@stonybrook.edu>
2022-12-04 18:50:28 -0500

    splashscreen done with redirect link

--- client/src/components/SplashScreen.js
+++ client/src/components/SplashScreen.js
@@ -1,7 +1,31 @@
+import Avatar from '@mui/material/Avatar';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+import Link from '@mui/material/Link';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+
 export default function SplashScreen() {
+    const handleLogin = () => {
+        window.location.href = 'http://localhost:3000/login'
+    }
+    const handleRegister = () => {        
+        window.location.href = 'http://localhost:3000/register'
+    }
     return (
         <div id="splash-screen">
-            Playlister
+           <Typography sx={{fontSize:'60px',height:"20%", fontWeight:'bold'}}>Playlister</Typography>
+           <Typography sx={{fontSize:'30px',fontWeight:'bold',height:"10%"}}>Welcome to Playlister!</Typography>
+           <Typography sx={{fontSize:'20px'}}>Let Fill the Life with Music</Typography>
+           <Typography sx={{fontSize:'20px'}}>Sign in to continue to Playlister</Typography>
+           <Typography sx={{fontSize:'15px'}}>Developed by Haoshui Gao </Typography>
+           <Button onClick={handleLogin}   variant="contained" sx={{bottom:300, right:40,}}>Login</Button>
+           <Button onClick={handleRegister}  variant="contained" sx={{bottom:300, }}>Create Account</Button>
+           <Button variant="contained" sx={{bottom:300, left:40}}>Continue as Guest</Button>
         </div>
     )
 }
